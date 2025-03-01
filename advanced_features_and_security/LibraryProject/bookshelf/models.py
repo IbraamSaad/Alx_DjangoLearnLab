f2a75918183a7f63d3f2ac4e13d0905f5156495b
IbraamSaad <ebramb37@gmail.com>
2025-03-01 18:30:09 +0200

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+lass CustomUser(AbstractUser):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_photo/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+        
 class Book(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.CharField(max_length=100)
