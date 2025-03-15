57954d3135dbfe8aa7e181470b426e51a5a2ff5e
IbraamSaad <ebramb37@gmail.com>
2025-03-16 00:08:41 +0200

    Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
 from django.urls import reverse_lazy
 from .models import Book
+from .serializers import BookSerializer
+
 
 class BookListView(ListView):
-    model = Book
-    template_name = 'book_list.html'  # Create this template
-    context_object_name = 'books'
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']  # Fields for filtering
+    search_fields = ['title', 'author']  # Fields for searching
+    ordering_fields = ['title', 'publication_year']  # Fields for ordering
+
 
 class BookDetailView(DetailView):
     model = Book
