8c8fd12a28dcd2cbfa96320e4b24ce44efbe04a2
IbraamSaad <ebramb37@gmail.com>
2025-03-16 00:18:51 +0200

    adding testing unit

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,16 +1,15 @@
+# advanced_api_project/books/tests/test_views.py
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from .models import Book
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 class BookAPITestCase(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.client.login(username='testuser', password='testpassword') #use login here
 
         self.book1 = Book.objects.create(title='Test Book 1', author='Test Author 1', publication_year=2020)
         self.book2 = Book.objects.create(title='Test Book 2', author='Test Author 2', publication_year=2021)
@@ -69,21 +68,21 @@ class BookAPITestCase(APITestCase):
         self.assertEqual(response.data[0]['title'], 'Test Book 2')
 
     def test_unauthenticated_create(self):
-        self.client.credentials() #remove auth
+        self.client.logout() #logout to test unauthenticated
         url = reverse('book-create')
         data = {'title': 'New Book', 'author': 'New Author', 'publication_year': 2023}
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN) #403 is more accurate for session auth.
 
     def test_unauthenticated_update(self):
-        self.client.credentials() #remove auth
+        self.client.logout()
         url = reverse('book-update', args=[self.book1.pk])
         data = {'title': 'Updated Book', 'author': 'Updated Author', 'publication_year': 2022}
         response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_unauthenticated_delete(self):
-        self.client.credentials() #remove auth
+        self.client.logout()
         url = reverse('book-delete', args=[self.book1.pk])
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
