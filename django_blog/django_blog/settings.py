a328fde8738d89a8f5095a1daf7e13cd26c3e9a8
IbraamSaad <ebramb37@gmail.com>
2025-03-23 01:36:17 +0200

    database config

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,7 +80,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'PORT': '3306',
+        'USER':
     }
 }
 
