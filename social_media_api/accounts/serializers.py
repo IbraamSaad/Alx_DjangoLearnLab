89d3b1120e304e0a8d4b0e90a92df741903055da
IbraamSaad <ebramb37@gmail.com>
2025-03-28 13:58:32 +0200

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+"Token.objects.create", "get_user_model().objects.create_user"
 
 User = get_user_model()
 
