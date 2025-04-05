6042a16797d7546860d0f2c97d59351895b056dc
IbraamSaad <ebramb37@gmail.com>
2025-04-06 01:17:50 +0200

    override the django default user

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import User
 
 # creating custom user and override the default behaviour for the django deault user
 class CustomUser(AbstractUser):
